2178fa1cb31a46569c2760134978a224113b10b0
Bioust <markabubakar0@gmail.com>
2024-11-30 17:47:03 +0530

    End of section 2

---
+++ 12.cs
@@ -0,0 +1,11 @@
+//Entity framework is ORM object relational manager
+//Translatesd our code into SQL commands that update our tables in the database.
+//Querying DB
+//Keep track of changes
+//Saving insert update delete
+//Concurrency update
+//Transaction management
+//Caching
+//Built in conventions
+//COntigure entities
+//Migrations
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    //Logical name of a class
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        //String in.Net is concidered as reference type and int is concidered as primitive type
+        //Default value of primitive type is 0 while as for reference type is null when nullable is accetable
+        
+    }
+}
